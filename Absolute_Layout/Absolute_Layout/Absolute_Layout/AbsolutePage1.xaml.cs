dbc2575ae68e677c26ca80f42e28c237d275ba1f
grferreira96 <guikarate-do@hotmail.com>
2018-04-24 16:36:00 -0300

    Absolute_Layout

---
+++ Absolute_Layout/Absolute_Layout/Absolute_Layout/AbsolutePage1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Absolute_Layout
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AbsolutePage1 : ContentPage
+	{
+		public AbsolutePage1 ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
