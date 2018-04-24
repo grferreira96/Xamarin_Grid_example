4bb14ecb19d48d9e1177ca3d911c6e9055c90065
grferreira96 <guikarate-do@hotmail.com>
2018-04-24 16:05:54 -0300

    Images_app

---
+++ Images_app/Images_app/Images_app/ImageExercise.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Images_app
+{
+    
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ImageExercise : ContentPage
+    {
+        private int currentImage = 1;
+        public ImageExercise()
+        {
+            InitializeComponent();
+            currentImage = 1;
+            LoadImage();
+        }
+        void LoadImage()
+        {
+            image.Source = new UriImageSource
+            {
+                Uri = new Uri(String.Format("http://lorempixel.com/1920/1080/city/{0}", currentImage)),
+                CachingEnabled=false
+            };
+        }
+        void Previous_Clicked(object sender, System.EventArgs e)
+        {
+            currentImage--;
+            if (currentImage == 0)
+                currentImage = 10;
+            LoadImage();
+
+        }
+        void Next_Clicked(object sender, System.EventArgs e)
+        {
+            currentImage++;
+            if (currentImage == 11)
+                currentImage = 1;
+            LoadImage();
+        }
+    }
+}
---
+++ Images_app/Images_app/Images_app/MarkupExtensions/EmbeddedImage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Images_app.MarkupExtensions
+{
+    [ContentProperty("ResourceId")]
+    class EmbeddedImage : IMarkupExtension
+    {
+        public string ResourceId { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (String.IsNullOrWhiteSpace(ResourceId))
+                return null;
+
+            return ImageSource.FromResource("Images_app.Images.background.jpg");
+        }
+    }
+}
